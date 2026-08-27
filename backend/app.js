6374640c6cf0caabdd487c9fe1ab38c6ea3efcb9
owlnikita <hrymkanom@gmail.com>
2026-08-27 15:04:25 +0500

    added routing and cors middleware for local development

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,14 @@
 const config = require('./config/index');
 const express = require('express');
+const cors = require('cors');
+require('./database/schema');
+
+const bookingRoutes = require('./routes/bookings.routes');
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use('/api/bookings', bookingRoutes);
 
 if (config.isDebug) {
     console.log("DEBUG is ON");
